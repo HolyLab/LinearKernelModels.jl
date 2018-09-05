c00d12f0c17bc1d564d8994bfbb26b5882c84d88
Tim Holy <tim.holy@gmail.com>
2018-09-05 12:39:04 -0500

    Redesign to allow arbitrary time axis for kernel

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,19 +4,20 @@ using LinearKernelModels
 else
     using Test
 end
+using OffsetArrays
 
 @testset "Single stimulus" begin
     S = rand(100)
-    ktrue = randn(5)
+    ktrue = OffsetArray(randn(5), -4:0)
     r = compute_r(S, ktrue)
     C, d = compute_Cd(S, r, 5)
-    kcalc = C \ d
+    kcalc = reshape(C \ d, Compat.axes(ktrue))
     @test kcalc ≈ ktrue
     nfailed = 0
     for i = 1:100
         rnoisy = r .+ randn(size(r))
         C, d = compute_Cd(S, rnoisy, 5)
-        knoisy = C \ d
+        knoisy = reshape(C \ d, Compat.axes(ktrue))
         kerr = compute_kerr(knoisy, C, S, rnoisy)
         nfailed += any((knoisy .- ktrue).^2 .> 3*kerr)
     end
@@ -28,23 +29,32 @@ end
         r = compute_r(S, ktrue)
         rnoisy = r .+ randn(size(r))
         C, d = compute_Cd(S, rnoisy, 5)
-        knoisy = C \ d
+        knoisy = reshape(C \ d, Compat.axes(ktrue))
         kerr = compute_kerr(knoisy, C, S, rnoisy)
         nfailed += any((knoisy .- ktrue).^2 .> 3*kerr)
     end
     @test nfailed <= 5
 end
 
+@testset "Single acausal stimulus" begin
+    S = rand(100)
+    ktrue = OffsetArray(randn(5), -2:2)
+    r = compute_r(S, ktrue)
+    C, d = compute_Cd(S, r, Compat.axes(ktrue, 1))
+    kcalc = reshape(C \ d, Compat.axes(ktrue))
+    @test kcalc ≈ ktrue
+end
+
 @testset "Multiple stimuli and NaNs" begin
     S = rand(100, 3)
-    ktrue = randn(5, 3)
+    ktrue = OffsetArray(randn(5, 3), -4:0, 1:3)
     r = compute_r(S, ktrue)
     C, d = compute_Cd(S, r, 5)
-    kcalc = reshape(C \ d, (5, 3))
+    kcalc = reshape(C \ d, Compat.axes(ktrue))
     @test kcalc ≈ ktrue
 
     r[5:10] = r[50:52] = NaN
     C, d = compute_Cd(S, r, 5)
-    kcalc = reshape(C \ d, (5, 3))
+    kcalc = reshape(C \ d, Compat.axes(ktrue))
     @test kcalc ≈ ktrue
 end
